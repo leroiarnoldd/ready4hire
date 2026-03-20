299fd9a173f1f5713e99c5adbe0d61c924515747
leroiarnoldd <leroiarnoldd@gmail.com>
2026-03-20 23:28:38 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
-
-// Hide loading spinner
-const loading = document.getElementById('loading');
-if(loading) loading.style.display = 'none';
