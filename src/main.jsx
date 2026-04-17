b6d5bdde90dc4c87e8e55aba1814723678a83bb4
leroiarnoldd <leroiarnoldd@gmail.com>
2026-04-17 23:12:09 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Ready4Hire.jsx'
+
+document.getElementById('loading')?.remove()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
