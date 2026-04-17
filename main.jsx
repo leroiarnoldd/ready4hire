909735c97bc0ef8f170d3bfbc78c8407e9a6f30d
leroiarnoldd <leroiarnoldd@gmail.com>
2026-04-17 23:08:36 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Ready4Hire.jsx'
+
+document.getElementById('loading')?.remove()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
