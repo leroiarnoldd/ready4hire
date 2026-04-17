29aa7af6a625b74a36f1089a899b8cf388200ad3
leroiarnoldd <leroiarnoldd@gmail.com>
2026-04-17 23:34:55 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from 'vite-plugin-commonjs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [commonjs(), react()],
   build: {
     outDir: 'dist',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    chunkSizeWarningLimit: 10000,
   },
 })
