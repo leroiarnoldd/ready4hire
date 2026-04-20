b145af11ed031bdc0102db4edfb046873d0b363d
leroiarnoldd <leroiarnoldd@gmail.com>
2026-04-20 21:51:25 +0100

    Update Ready4Hire.jsx

--- Ready4Hire.jsx
+++ Ready4Hire.jsx
@@ -2907,11 +2907,6 @@ if(cn){
   setSt("found");
   onFound(pc.trim().toUpperCase(),detectPCKey(pc));
 } else setSt("err");
-
-  }catch(e){}
-  onFound(pc.trim().toUpperCase(),detectPCKey(pc));
-} else setSt("err");
-  };
   return(
     <div>
       <div style={{display:"flex",gap:8}}>
